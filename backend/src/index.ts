c23a7beb34a7c92af062eb959cfad814635c2bc5
GABRIEL-SANCHES-V <79290891+GABRIEL-SANCHES-V@users.noreply.github.com>
2025-09-17 18:07:09 -0300

    Rotas de acesso para criação de usaurios e rankings dos usuarios e limpar usuaruios feitas atraves da biblioteca express

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,17 @@
-import pool from "./database/connectionDB.js";
+import dotenv from "dotenv";
+import express from "express";
+import userRouter from "./router/userRouter.js";
+
+dotenv.config({quiet: true });
+
+const app = express();
+app.use(express.json());
+
+app.use("/api", userRouter);
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+export default app;
